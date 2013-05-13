59d4023ee12a8ad48739b4955fe60e9633812be0
Sho Hashimoto <hashimoto@shokai.org>
2013-05-14 03:53:57 +0900

    bundle update, MiniTest::Test

--- test/test_websocket_client_simple.rb
+++ test/test_websocket_client_simple.rb
@@ -1,6 +1,6 @@
 require File.expand_path 'test_helper', File.dirname(__FILE__)
 
-class TestWebSocketClientSimple < MiniTest::Unit::TestCase
+class TestWebSocketClientSimple < MiniTest::Test
 
   def port
     (ENV['WS_PORT'] || 18080).to_i
