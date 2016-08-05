fe767b97561bce6be72ed3bd1f3344057d960608
Brandon Dewitt <brandonsdewitt@gmail.com>
2016-08-05 16:59:55 -0600

    use non_block write and read methods and make ssl work with non_block .... also update send => send_data

--- lib/websocket-client-simple/client.rb
+++ lib/websocket-client-simple/client.rb
@@ -34,50 +34,30 @@ module WebSocket
             @socket = ::OpenSSL::SSL::SSLSocket.new(@socket, ssl_context)
             @socket.connect
           end
+
           @handshake = ::WebSocket::Handshake::Client.new :url => url, :headers => options[:headers]
           @handshaked = false
           @pipe_broken = false
-          frame = ::WebSocket::Frame::Incoming::Client.new
           @closed = false
           once :__close do |err|
             close
             emit :close, err
           end
 
-          @thread = Thread.new do
-            while !@closed do
-              begin
-                unless recv_data = @socket.getc
-                  sleep 1
-                  next
-                end
-                unless @handshaked
-                  @handshake << recv_data
-                  if @handshake.finished?
-                    @handshaked = true
-                    emit :open
-                  end
-                else
-                  frame << recv_data
-                  while msg = frame.next
-                    emit :message, msg
-                  end
-                end
-              rescue => e
-                emit :error, e
-              end
-            end
-          end
-
-          @socket.write @handshake.to_s
+          handshake
+          @thread = poll
         end
 
-        def send(data, opt={:type => :text})
-          return if !@handshaked or @closed
-          type = opt[:type]
-          frame = ::WebSocket::Frame::Outgoing::Client.new(:data => data, :type => type, :version => @handshake.version)
+        def send_data(data, opt={:type => :text})
+          return if !@handshaked || @closed
+
+          frame = ::WebSocket::Frame::Outgoing::Client.new(:data => data, :type => opt[:type], :version => @handshake.version)
+
           begin
-            @socket.write frame.to_s
+            @socket.write_nonblock(frame.to_s)
+          rescue IO::WaitWritable, Errno::EINTR
+            IO.select(nil, [@socket])
+            retry
           rescue Errno::EPIPE => e
             @pipe_broken = true
             emit :__close, e
@@ -86,22 +66,78 @@ module WebSocket
 
         def close
           return if @closed
-          if !@pipe_broken
-            send nil, :type => :close
-          end
+
+          send_data nil, :type => :close if !@pipe_broken
+          emit :__close
+        ensure
           @closed = true
           @socket.close if @socket
           @socket = nil
-          emit :__close
           Thread.kill @thread if @thread
+        end
+
+        def handshake
+          @socket.write @handshake.to_s
+
+          while !@handshaked
+            begin
+              read_sockets, _, _ = IO.select([@socket], [], [], 10)
+
+              if read_sockets && read_sockets[0]
+                @handshake << @socket.read_nonblock(1024)
+
+                if @socket.respond_to?(:pending) # SSLSocket
+                  @handshake << @socket.read(@socket.pending) while @socket.pending > 0
+                end
+
+                @handshaked = @handshake.finished?
+              end
+            rescue IO::WaitReadable
+              # No op
+            rescue IO::WaitWritable
+              IO.select(nil, [socket])
+              retry
+            end
+          end
+        end
+
+        def poll
+          return Thread.new(@socket) do |socket|
+            frame = ::WebSocket::Frame::Incoming::Client.new
+            emit :open
+
+            while !@closed do
+              read_sockets, _, _ = IO.select([socket], nil, nil, 1)
+
+              if read_sockets && read_sockets[0]
+                begin
+                  frame << socket.read_nonblock(1024)
+
+                  if socket.respond_to?(:pending)
+                    frame << socket.read(socket.pending) while socket.pending > 0
+                  end
+
+                  if msg = frame.next
+                    emit :message, msg
+                    frame = ::WebSocket::Frame::Incoming::Client.new
+                  end
+                rescue IO::WaitReadable
+                  # Nothing
+                rescue IO::WaitWritable
+                  IO.select(nil, [socket])
+                  retry
+                rescue => e
+                  emit :error, e
+                end
+              end
+            end
+          end
         end
 
         def open?
           @handshake.finished? and !@closed
         end
-
       end
-
     end
   end
 end
